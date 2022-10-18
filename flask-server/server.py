e0b6735c6ce3cc535355b40f06c24ba6bc142d4e
Chris Mott <chrismott@uiowa.edu>
2022-10-18 11:05:05 -0500

    SQLAlchemy Initialized

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 # create app instance
 app = Flask(__name__)
+
+# base directory for database
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# database
+app.config["SQLALCHEMY_DATATBASE_URI"] = "sqlite:///" + os.path.join(basedir, 'db.sqlite')
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # stop console from complaining OPTIONAL
+
+# init db
+db = SQLAlchemy(app)
+
+
 
 # API route
 @app.route('/')
