28059309106d3427d82d43bf1729143b803b486d
Chris Mott <chrismott@uiowa.edu>
2022-10-17 22:26:39 -0500

    created first example of retrieving data from flask api to be available to front end. Accomplished using useState and useEffect hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
 function App() {
+  // 'data' is the actual variable and setData is the function we can use to manipulate the state of the 'data' variable. 
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/members").then(
+        res => res.json()   // put the response the /member route gives us gets put into json
+    ).then(
+        data => {           // whatever data is inside the json gets put into the 'data' variable using the setData function
+            setData(data)   
+            console.log(data) // to make sure the api fetch worked we console log to see if the data was retrieved
+        }
+    )
+  }, []) // pass in empty array with useEffect block to ensure it only runs once
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
