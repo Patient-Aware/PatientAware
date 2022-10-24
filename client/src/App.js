9bddd1c85475e0935d1e8edd4f08fafc542fe6b4
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-10-24 17:53:46 -0500

    Add AppBar to top of page

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,19 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
 
 function App() {
-  // 'data' is the actual variable and setData is the function we can use to manipulate the state of the 'data' variable. 
-  const [data, setData] = useState([{}])
-
-  useEffect(() => {
-    fetch("/members").then(
-        res => res.json()   // put the response the /member route gives us gets put into json
-    ).then(
-        data => {           // whatever data is inside the json gets put into the 'data' variable using the setData function
-            setData(data)   
-            console.log(data) // to make sure the api fetch worked we console log to see if the data was retrieved
-        }
-    )
-  }, []) // pass in empty array with useEffect block to ensure it only runs once
-
-
+  
   return (
     <div>
-
-        {(typeof data.members === 'undefined') ? ( // if members == undefined that means the api is being fetched and we don't have the members yet
-            <p>Loading...</p>
-        ) : (
-            data.members.map((member, i) => ( // if members are available, we map every member to a <p> tag
-                <p key={i}>{member}</p>
-            ))
-        )}
+        <AppBar position='static'>
+            <Toolbar>
+                <Typography variant='h5' component="div">
+                    PatientAware
+                </Typography>
+            </Toolbar>
+        </AppBar>
     </div>
   )
 }
