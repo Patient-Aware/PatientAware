83221d7fb9c128147ab412a707c4d57dba81c190
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-10-24 19:41:45 -0500

    Add start test button to home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,25 @@
 import * as React from 'react'
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
+
 
 export default function Home() {
     return (
         <Container fixed>
-            <Typography variant='h1'>
-                Welcome to PatientAware
-            </Typography>
+            <Stack
+                direction="column"
+                justifyContent="space-between"
+                alignItems="center"
+                spacing={8}
+            >
+                <Typography variant='h1'>
+                    Welcome to PatientAware
+                </Typography>
+                <Button variant='contained' size='large' sx={{ fontSize: '2em' }}>Start Test</Button>
+            </Stack>
+
         </Container>
     )
 }
