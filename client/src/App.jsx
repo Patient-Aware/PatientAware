730b191f335604cf59aebee0f901099edab8050a
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-11-15 12:53:24 -0600

    add theme to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,21 +4,42 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 import { Outlet } from "react-router-dom"
+import { createTheme, ThemeProvider } from "@mui/material"
 
 function App() {
   
+
+  const themeOptions = {
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#25d897',
+      },
+      secondary: {
+        main: '#f50057',
+      },
+      text: {
+        primary: '#132f3d',
+      },
+    },
+  };
+
+  const patientAwareTheme = createTheme(themeOptions)
+
   return (
     <div>
-        <AppBar position='static'>
-            <Toolbar>
-                <Typography variant='h5' component="div">
-                    PatientAware
-                </Typography>
-            </Toolbar>
-        </AppBar>
-        <Box sx={{ mt: 2 }}>
-          <Outlet />
-        </Box>
+        <ThemeProvider theme={patientAwareTheme}>
+          <AppBar position='static'>
+              <Toolbar>
+                  <Typography variant='h5' component="div">
+                      PatientAware
+                  </Typography>
+              </Toolbar>
+          </AppBar>
+          <Box sx={{ mt: 2 }}>
+            <Outlet />
+          </Box>
+        </ThemeProvider>
     </div>
   )
 }
