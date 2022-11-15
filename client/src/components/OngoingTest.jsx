c539c87f3e6a56776ffc757df5195d38569e65c2
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-11-15 11:46:30 -0600

    Add instruction cards with placeholder content

--- client/src/components/OngoingTest.jsx
+++ client/src/components/OngoingTest.jsx
@@ -2,7 +2,11 @@ import React from "react"
 import { 
     Container,
     Stack,
-    Typography
+    Typography,
+    Grid,
+    Card,
+    CardMedia,
+    CardContent
 } from "@mui/material"
 
 export default function OngoingTest() {
@@ -18,7 +22,66 @@ export default function OngoingTest() {
                 <Typography variant='h3'>
                     Testing in Progress
                 </Typography>
+                <Typography variant='h5' color="text.secondary">
+                    Follow the instructions below to conduct the antigen test
+                </Typography>
             </Stack>
+
+            <Grid container sx={{ mt: 4 }} spacing={2}>
+                <Grid item xs={4}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            alt="Step 1 demonstration"
+                        />
+                        <CardContent>
+                            <Typography variant="h5" gutterBottom>
+                                Step 1
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Follow these instructions to perform step 1 of the testing process
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            alt="Step 2 demonstration"
+                        />
+                        <CardContent>
+                            <Typography variant="h5" gutterBottom>
+                                Step 2
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Follow these instructions to perform step 2 of the testing process
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+                <Grid item xs={4}>
+                    <Card>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            alt="Step 3 demonstration"
+                        />
+                        <CardContent>
+                            <Typography variant="h5" gutterBottom>
+                                Step 3
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Follow these instructions to perform step 3 of the testing process
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
         </Container>
     )
 }
