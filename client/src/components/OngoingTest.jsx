4a156556ee82d37b111dea5721c146b86d75ba71
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-11-15 12:33:20 -0600

    add TestStatus component

--- client/src/components/OngoingTest.jsx
+++ client/src/components/OngoingTest.jsx
@@ -8,6 +8,7 @@ import {
     CardMedia,
     CardContent
 } from "@mui/material"
+import TestStatus from "./TestStatus"
 
 export default function OngoingTest() {
 
@@ -17,7 +18,7 @@ export default function OngoingTest() {
                 direction="column"
                 justifyContent="space-between"
                 alignItems="center"
-                spacing={8}
+                spacing={4}
             >
                 <Typography variant='h3'>
                     Testing in Progress
@@ -25,63 +26,67 @@ export default function OngoingTest() {
                 <Typography variant='h5' color="text.secondary">
                     Follow the instructions below to conduct the antigen test
                 </Typography>
+
+                <Grid container sx={{ mt: 4 }} spacing={2}>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                alt="Step 1 demonstration"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" gutterBottom>
+                                    Step 1
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Follow these instructions to perform step 1 of the testing process
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                alt="Step 2 demonstration"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" gutterBottom>
+                                    Step 2
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Follow these instructions to perform step 2 of the testing process
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                height="140"
+                                alt="Step 3 demonstration"
+                            />
+                            <CardContent>
+                                <Typography variant="h5" gutterBottom>
+                                    Step 3
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    Follow these instructions to perform step 3 of the testing process
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+
+                    <Grid item xs={12} sx= {{ mt: 6 }}>
+                        <TestStatus />
+                    </Grid>
+                </Grid>
             </Stack>
-
-            <Grid container sx={{ mt: 4 }} spacing={2}>
-                <Grid item xs={4}>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            alt="Step 1 demonstration"
-                        />
-                        <CardContent>
-                            <Typography variant="h5" gutterBottom>
-                                Step 1
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Follow these instructions to perform step 1 of the testing process
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-
-                <Grid item xs={4}>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            alt="Step 2 demonstration"
-                        />
-                        <CardContent>
-                            <Typography variant="h5" gutterBottom>
-                                Step 2
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Follow these instructions to perform step 2 of the testing process
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-
-                <Grid item xs={4}>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            height="140"
-                            alt="Step 3 demonstration"
-                        />
-                        <CardContent>
-                            <Typography variant="h5" gutterBottom>
-                                Step 3
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                Follow these instructions to perform step 3 of the testing process
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
         </Container>
     )
 }
---
+++ client/src/components/TestStatus.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import {
+    LinearProgress,
+    Stack,
+    Typography
+} from "@mui/material"
+
+export default function TestStatus(props) {
+
+    const [progress, setProgress] = React.useState(0)
+    const [minutesLeft, setMinutesLeft] = React.useState(15)
+
+    React.useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress(progress + 5)
+            setMinutesLeft(minutesLeft - 1)
+        }, 6000)
+
+        return () => {
+            clearInterval(timer)
+        }
+    })
+
+    return (
+        <Stack>
+            <Typography variant="h6" gutterBottom>
+                Stage: { progress < 30 ? "Calibrating sensor" : "Testing sample" }
+            </Typography>
+            <Typography variant="subtitle1" color="text.secondary" gutterBottom>
+                Time remaining: { minutesLeft } minutes
+            </Typography>
+            <LinearProgress variant="determinate" value={progress}></LinearProgress>
+        </Stack>
+    )
+}
