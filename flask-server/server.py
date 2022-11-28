d436243ddbd82ff2471a25ae7ad3bd8d4603bb71
Chris Mott <chrismott@uiowa.edu>
2022-11-28 16:39:18 -0600

    clean up useless patient routes

--- flask-server/server.py
+++ flask-server/server.py
@@ -124,46 +124,6 @@ class AntigenSchema(ma.Schema):
 antigen_schema = AntigenSchema()
 antigens_schema = AntigenSchema(many=True)
 
-# patient Class/Model
-class Patient(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100))
-    last_name = db.Column(db.String(100))
-    dob = db.Column(db.DateTime)
-    #doctors = db.relationship('Doctor', backref='patient', lazy=True)
-
-    def __init__(self, first_name, last_name, dob):
-        self.first_name = first_name
-        self.last_name = last_name 
-        self.dob = dob
-
-# Patient Schema
-class PatientSchema(ma.Schema):
-    class Meta: # the fields we are allowed to show
-        fields = ('id', 'first_name', 'last_name', 'dob')#, 'doctors')
-
-# Initialize Schema
-patient_schema  = PatientSchema() #strict = True to rid of console warning
-patients_schema = PatientSchema(many=True) # we need schema for multiple patients. If we are fetching multiple patients we need this
-
-# Create a patient
-@app.route('/patient', methods=['POST'])
-def patient_create():
-    format = '%m/%d/%Y'
-
-    # read in the post information that was posted from the client (react)
-    first_name = request.json['first_name']
-    last_name = request.json['last_name']
-    dob = datetime.datetime.strptime(request.json['dob'], format)
-
-    # initialize a new object
-    new_patient = Patient(first_name, last_name, dob)
-
-    # add and commit new database entry
-    db.session.add(new_patient)
-    db.session.commit()
-
-    return patient_schema.jsonify(new_patient)
 
 @celery.task(bind = True)
 def sensor_read_task(self):
@@ -202,49 +162,6 @@ def start_test():
     return {"task_id" : task.id}
 
 
-# get all patients
-@app.route('/patient', methods=['GET'])
-def get_patients():
-    all_patients = Patient.query.all() # example of why sqlalchemy is helpful
-    result = patients_schema.dump(all_patients)
-    return jsonify(result)
-
-# get single patient
-@app.route('/patient/<id>', methods=['GET'])
-def get_patient(id):
-    patient = Patient.query.get(id) # example of why sqlalchemy is helpful
-    return patient_schema.jsonify(patient)
-
-# Update a patient
-@app.route('/patient/<id>', methods=['PUT'])
-def patient_update(id):
-    patient = Patient.query.get(id)
-
-    format = '%m/%d/%Y'
-
-    # read in the post information that was posted from the client (react)
-    first_name = request.json['first_name']
-    last_name = request.json['last_name']
-    dob = datetime.datetime.strptime(request.json['dob'], format)
-
-    #construct new product to submit to the database
-    patient.first_name = first_name
-    patient.last_name = last_name 
-    patient.dob = dob 
-
-    # commit new changes to database
-    db.session.commit()
-
-    return patient_schema.jsonify(patient)
-
-# Delete single patient
-@app.route('/patient/<id>', methods=['DELETE'])
-def patient_delete(id):
-    patient = Patient.query.get(id) # example of why sqlalchemy is helpful
-    db.session.delete(patient)
-    db.session.commit()
-    return patient_schema.jsonify(patient)
-
 
 # Create the tables
 with app.app_context(): # must be below the schema init
@@ -255,10 +172,6 @@ with app.app_context(): # must be below the schema init
 def hello_world():
     return "hello, world!"
 
-#another api route for example
-@app.route('/members')
-def members():
-    return {"members": ["Matt", "Chris", "Ralph", "Luke"]}
 
 @app.before_first_request
 def setup():
