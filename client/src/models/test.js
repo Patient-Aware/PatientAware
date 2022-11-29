50cf3f0a2b5515f9597eff701b1638b197082447
Ralph Nahra <ralph-nahra@uiowa.edu>
2022-11-29 00:54:52 -0600

    add mock test result values

--- client/src/models/test.js
+++ client/src/models/test.js
@@ -8,3 +8,21 @@ export async function getTest() {
     const test = JSON.parse(localStorage.getItem("test"))
     return test
 }
+
+export async function getResults() {
+
+    const test = await getTest()
+
+    const selectedAntigens = Object.values(test)
+        .filter(portSelection => portSelection !== "None")
+
+    // come up with some random values until backend is integrated
+    const antigenLevels = {
+        'CEA': Math.floor(Math.random() * 10 + 5),
+        'CA19-9': Math.floor(Math.random() * 10),
+        'KRAS': Number.parseFloat(Math.random() * 3).toFixed(2),
+        'BRAF V600E': Number.parseFloat(Math.random() * 0.5).toFixed(2)
+    }
+
+    return selectedAntigens.map(antigen => { return { antigen: antigen, detectedLevel: antigenLevels[antigen] } })
+}
